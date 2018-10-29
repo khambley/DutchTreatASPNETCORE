a5912fa81c04c1ebd123721bcb7c5d967b1b3916
Katherine Hambley <superdesigngirl@outlook.com>
2018-10-28 20:39:35 -0500

    Initial Commit

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,14 @@
+﻿console.log("Hello Pluralsight");
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
+// Not a good way to list out items in a collection, better way with jquery.
+//var listItems = productInfo.item[0].children;
